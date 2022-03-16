18f419ac42a1a1a17f3aac478080e402deab1b14
Artem <askolesnikov@miem.hse.ru>
2022-03-17 00:05:44 +0300

    merge in main2

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
 console.log('Hello world!')
 
-<<<<<<< HEAD
 const userAge = JS;
 if(userAge >= 18){
     console.log('Вы совершеннолетний.')
 } else {
     console.log('Вы не совершеннолетнийю')
-=======
-const age = 20;
-
-if (age >= 18){
-    console.log('Вы совершеннолетний!')
-} else {
-    console.log('Вы не совершеннолнтний!')
->>>>>>> feature/user-access
 }
